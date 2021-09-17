abeb7aeaaf5b6dacc823be01a3d49c9d72dcc8c9
guilhermeabel99 <gpeuvis@gmail.com>
2021-09-17 10:40:15 -0300

    initial dev

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.facebook.com/');
+
+  // Acessa página de login
+
+  await page.type('#email', 'guilhermeabel777@gmail.com');
+  // await page.type('#pass', 'gpeuvisteste@100');
+
+  await page.click('[type="submit"]')
+  
+
+
+  
+
+
+
+
+  
+})();
