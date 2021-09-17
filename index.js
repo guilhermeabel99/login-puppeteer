35da3564e630d0714ce5da9c7a4f66c89eedb817
guilhermeabel99 <gpeuvis@gmail.com>
2021-09-17 11:01:07 -0300

    initial dev

--- index.js
+++ index.js
@@ -8,11 +8,40 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://www.facebook.com/');
 
-  // Acessa página de login
 
-  await page.type('#email', 'guilhermeabel777@gmail.com');
-  // await page.type('#pass', 'gpeuvisteste@100');
+  // Troque os valores de process.env.UNSPLASH_EMAIL e process.env.UNSPLASH_PASS pelo seu login e senha :)
+  await page.type('#email', 'guilhermeabel777@gmail.com')
+  await page.type('#pass', 'teste@100')
 
   await page.click('[type="submit"]')
-  
+
+
+  // await browser.close();
 })();
+
+
+
+
+
+
+
+
+
+// require('dotenv').config();
+// const puppeteer = require('puppeteer');
+
+// (async () => {
+//   const browser = await puppeteer.launch({
+//     headless: false,
+//   });
+//   const page = await browser.newPage();
+//   await page.goto('https://www.facebook.com/');
+
+//   // Acessa página de login
+
+//   await page.type('#email', 'guilhermeabel777@gmail.com');
+//   // await page.type('#pass', 'gpeuvisteste@100');
+
+//   await page.click('[type="submit"]')
+  
+// })();
